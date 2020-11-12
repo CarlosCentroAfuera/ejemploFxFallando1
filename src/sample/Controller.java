27472e948a5b57dd36530fd7d237d3945c253d9d
Carlos <carlostena@centroafuera.es>
2020-11-12 11:26:01 +0100

    Perro añadido con listeners

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
 
-    public void mostrarPerro(){
-        Perro perro = new Perro();
-        // Mostra el perror por pantalla.
+    Perro perro = new Perro();
+
+    String nombre = "";
+
+    @FXML
+    private Label idLabelEdad;
+    @FXML
+    private Label idLabelNombre;
+
+    @FXML
+    public void onEdadClicked(MouseEvent event){
+        idLabelEdad.setText(perro.edad.toString());
+    }
+
+    @FXML
+    public void onNombreClicked(){
+        nombre += perro.nombre;
+        idLabelNombre.setText(nombre);
     }
 }
